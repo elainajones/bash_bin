aac999b6afcb13d203586fd63165fc0ab0d0aea8
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2024-01-02 20:14:51 -0800

    style: Format date for rootfs backup
    Formats the date from YYYY-mm-dd to YYYYmmdd because I think it
    looks better.

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 rm -rf var/cache/*
-tar -cvf $(cat etc/hostname)-rootfs-$(date +%F).tar bin boot dev etc lib lib64 media mnt opt proc root run sbin swapfile sys tmp usr var
+tar -cvf $(cat etc/hostname)-rootfs-$(date +%Y%m%d).tar \
+    bin boot dev etc lib lib64 media mnt opt proc root run sbin swapfile sys tmp usr var
 
