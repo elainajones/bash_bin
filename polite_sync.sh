11ba3d26d9786ad82ab6c468a125537dbeb8e22f
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2024-01-30 01:45:40 -0800

    feat: Add emerge --sync script for Gentoo
    Adds emerge --sync script to update repos on Gentoo systems. This
    checks that the repos were not synced within 2 days before syncing
    as per gentoo-netiquette which says to sync not more than once a
    day. Intended to be added to ~/.bashrc.

---
+++ polite_sync.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+get_sync_date() {
+    target="/var/db/repos/gentoo";
+    mod_date=$(ls -ldqgG --time-style="+%Y%m%d%H%M%S" $target | \
+        grep -oP "\S+(?=\s$target)");
+
+    echo $mod_date;
+}
+
+main() {
+    # Limit in "YYYYmmddHHMMSS" format.
+    sync_limit="00000002000000";
+    # Log path to check sync messages
+    # TODO: Check log to determine if portage
+    #   should be updated. Update automatically.
+    log_path="emerge-sync-$(date "+%F").log"
+
+    today=$(date "+%Y%m%d%H%M%S");
+    sync_date=$(get_sync_date);
+    # Time means duration. Not the same as date.
+    sync_time=$((today - sync_date));
+
+    if [ $sync_time -gt $sync_limit ]; then
+        sudo emerge --sync &>$log_path;
+    fi
+}
+main;
