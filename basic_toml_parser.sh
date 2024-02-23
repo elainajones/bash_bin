931b7de004260933ce8562ce45c86f3c7898e465
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2024-02-23 04:40:15 -0800

    fix: Improve parsing logic
    Improves parsing match patterns to avoid matching non/keys if key
    strings appear elsewhere in the table string. For example, a keyname
    of 'f' may match the first occurance of the letter 'f' rather
    than the intended key. This is fixed by requiring the key to be
    directly followed by an '=' sign. Adds optional matching of
    whitespace around the '=' sign to avoid parsing errors when no
    padding is provided.

--- basic_toml_parser.sh
+++ basic_toml_parser.sh
@@ -29,51 +29,56 @@ parse_toml() {
     declare config_path=$1;
     declare -gA CONFIG=();
     # Filter out comments so they aren't interpreted.
-    declare lines=$(grep -oP ^"[^#].+" $config_path);
+    declare lines="$(grep -oP ^"[^#].+" $config_path)";
     # Match everything between `[` and `]` as table headers.
-    #declare headers=$(echo $lines | grep -oP "(?<=\[)\S+?(?=\])");
     declare headers=($(grep -oP "(?<=^\[)\S+?(?=\])" $config_path));
     for i in $(seq ${#headers[@]}); do
         h=${headers[$((i-1))]};
         next=${headers[$i]};
         # Use headers to match individual tables.
         #declare table=$(echo $lines | grep -oP "\[$h(\n|.)+?(?=(\[|\Z))");
-        if [[ $next ]]; then
-            declare table=$(echo $lines | \
-                grep -oP "\[$h(\n|.)+?(?=(\[$next|\Z))");
+        if [[ "$next" ]]; then
+            declare table="$(echo $lines | \
+                grep -oP "\[$h(\n|.)+?(?=(\[$next|\Z))")";
         else
-            declare table=$(echo $lines | \
-                grep -oP "\[$h(\n|.)+?(?=(\Z))");
+            declare table="$(echo $lines | \
+                grep -oP "\[$h(\n|.)+?(?=(\Z))")";
         fi
         # Match strings on the left of `=` sign as variables.
-        declare keys=($(echo $table | grep -oP "\S+\s?(?=\=)"));
-        for i in $(seq ${#keys[@]}); do
-            declare key=${keys[$((i-1))]};
+        declare keys=($(\
+            echo $table | \
+            grep -oP "\S+\s?(?==)" | \
+            grep -oP "\S.*" | grep -oP ".*\S" \
+        ));
+        # Iterate through table string, matching everything between 
+        # key and following key as key value.
+        for i in $(seq 1 ${#keys[@]}); do
+            declare key="${keys[$((i-1))]}";
             # 1. Use variable keys to match everything up to the next 
             #    variable key as the variable value.
             # 2. Match everything to the right of the `=` sign.
             # 3. Remove leading/trailing whitespace.
 	        # 4. Remove quotes from strings conditionally.
-            declare val=$(\
+            declare val="$(\
                 echo $table | \
-                grep -oP "(?<=${key}).+?(?=${keys[$i]}(\s=|\Z))" | \
-                grep -oP "(?<=\=).+" | \
+                grep -oP "(?<=${key})\s?=.+?(?=${keys[$i]}(\s?=|\Z))" | \
+                grep -oP "(?<==).+" | \
                 grep -oP "\S.*" | grep -oP ".*\S" | \
                 grep -oP "(?<=\"|\'|\b).+(?=\"|\'|\b)" \
-            );
+            )";
 
-            if ! [[ $val ]]; then
+            if ! [[ "$val" ]]; then
                 # Don't save undefined variables.
                 continue;
-            elif [[ ${CONFIG[$h]} ]]; then
+            elif [[ "${CONFIG[$h]}" ]]; then
                 # Append key if key list is not empty.
-                CONFIG[$h]+=" ${key}";
+                CONFIG["$h"]+=" ${key}";
             else
                 # Write key list with first key.
-                CONFIG[$h]=$key;
+                CONFIG["$h"]="$key";
             fi
             # Define `header.key=value` keypairs.
-            CONFIG["${h}.${key}"]=$val;
+            CONFIG["${h}.${key}"]="$val";
         done
     done
 }
